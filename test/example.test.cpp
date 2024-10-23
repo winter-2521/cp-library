666ecbeffdd46e22c70af86d519179a4abb608f7
winter-2521 <yuu.winter25@gmail.com>
2024-10-23 21:07:26 +0900

    add test example

---
+++ test/example.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/4/ITP1_4_A"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b; cin >> a >> b;
+    cout << fixed << setprecision(10);
+    cout << a/b << " " << a%b << " " << (long double)a/b << endl;
+    return 0;
+}
